b9e1d0fa00c6a1fb18706175671312f0a0a9c90a
Christoffer Winterkvist <chris@zenangst.com>
2014-10-13 20:34:34 +0200

    Rename test file

---
+++ NSString-HYPFormula/NSString_HYPFormulaTests.m
@@ -0,0 +1,31 @@
+//
+//  NSString_HYPFormula.m
+//  NSString-HYPFormula
+//
+//  Created by Christoffer Winterkvist on 13/10/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+#import "NSString+HYPFormula.h"
+
+@interface NSString_HYPFormula : XCTestCase
+
+@end
+
+@implementation NSString_HYPFormula
+
+- (void)testFormulaString
+{
+    NSDictionary *dictionary = {
+        "first_name" : "John",
+        "last_name"  : "Hyperseed"
+    };
+
+    NSString *formula = @"first_name last_name";
+
+    NSLog(@"formula: %@", [formula processValues:dictionary]);
+}
+
+@end
