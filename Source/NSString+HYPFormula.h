615b7d9d0f89f2a57b19d959d79441f68bf11893
Christoffer Winterkvist <chris@zenangst.com>
2014-10-13 20:20:12 +0200

    Add empty category

---
+++ Source/NSString+HYPFormula.h
@@ -0,0 +1,13 @@
+//
+//  NSString+HYPFormula.h
+//  HYPFormula
+//
+//  Created by Christoffer Winterkvist on 13/10/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (HYPFormula)
+
+@end
---
+++ Source/NSString+HYPFormula.m
@@ -0,0 +1,13 @@
+//
+//  NSString+HYPFormula.m
+//  HYPFormula
+//
+//  Created by Christoffer Winterkvist on 13/10/14.
+//
+//
+
+#import "NSString+HYPFormula.h"
+
+@implementation NSString (HYPFormula)
+
+@end
