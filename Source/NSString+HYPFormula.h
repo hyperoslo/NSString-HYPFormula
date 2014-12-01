970ce73373a407ca508250b34589d2e4722ab2b6
Elvis Nuñez <NSElvis@users.noreply.github.com>
2014-12-01 12:52:25 +0100

    Remove headers

--- Source/NSString+HYPFormula.h
+++ Source/NSString+HYPFormula.h
@@ -1,12 +1,4 @@
-//
-//  NSString+HYPFormula.h
-//  HYPFormula
-//
-//  Created by Christoffer Winterkvist on 13/10/14.
-//
-//
-
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 @interface NSString (HYPFormula)
 
